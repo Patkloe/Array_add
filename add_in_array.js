97394447ad266b736a4551fda151035c9a66c417
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-07-06 07:24:32 -0500

    Update add_in_array.js

--- add_in_array.js
+++ add_in_array.js
@@ -1,3 +1,12 @@
 var tab = [2,3,4];   // init array
 tab = [1,...tab];    // add the element 1, using the spreading operator
 tab;
+/*
+var tab = [2,3,4];
+var len1 = tab.length;
+var tab2 = [1,...tab];
+var len2 = tab2.length;
+tab2;
+console.log('\n');
+console.log("first size :" + " " +len1 + " " +"second size : " + " " +len2 );
+*/
